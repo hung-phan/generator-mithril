7192d74a78890e5fbb6105115758552f2cc6e3cb
hung-phan <colorvisavn@gmail.com>
2014-06-29 10:31:04 +0700

    remove grunt-bower-install

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -265,9 +265,6 @@ module.exports = function(grunt) {
             }
         },
 
-        // Automatically inject Bower components into the HTML file
-        'bower-install': { app: { html: '<%%= yeoman.app %>/index.html', ignorePath: '<%%= yeoman.app %>/' } },
-
         // Renames files for browser caching purposes
         rev: {
             dist: {
